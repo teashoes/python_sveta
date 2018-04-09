1842e45b9ca6caafa553ae6a731d53986ee198b2
Svetlana <stishkova93@gmail.com>
2018-04-10 00:46:24 +0300

    Delete add_noun_db.py

--- add_noun_db.py
+++
@@ -1,9 +0,0 @@
-from database import db_session, Noun
-from noun_artikel import get_art
-noun='moeder'
-
-nounadd=Noun(noun_name=noun, artikel=get_art(noun))
-
-db_session.add(nounadd)
-db_session.commit()
-print(nounadd.noun_name)
