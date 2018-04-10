059838761e629c09d724e23344806978b58a7631
Svetlana <stishkova93@gmail.com>
2018-04-10 11:08:21 +0300

    Delete wrt_csv.py

--- wrt_csv.py
+++
@@ -1,9 +0,0 @@
-#import data
-import csv
-
-with open('export.csv', 'w', encoding='utf-8') as f:
-    fields = ['first_name', 'last_name', 'email', 'gender', 'balance']
-    writer = csv.DictWriter(f, fields, delimiter=';')
-    writer.writeheader()
-    for user in user_list:
-        writer.writerow(user)
