be197469e7f0c192a4f6e0d12b417009b85cd542
Svetlana <stishkova93@gmail.com>
2018-04-18 20:43:05 +0300

    Update extract_conj.py

--- extract_conj.py
+++ extract_conj.py
@@ -1,6 +1,7 @@
 from database import db_session, Verb
 from sqlalchemy import and_, or_, not_
 from add_verb_db import verb_add
+from add_noun_db import noun_add
 from sqlalchemy import func
 
 def conj_show(verb, tense):
@@ -14,5 +15,16 @@ def conj_show(verb, tense):
         for row in Verb.query.filter(and_(Verb.verbname.like(verb), Verb.tense_id==tense)):
             reply.append(row.pronoun+' '+row.converb)
     return reply
+
+def noun_show(noun):
+    reply=[]
+    if Noun.query.filter(Noun.noun_name.like(noun)).count() != 0:
+        for row in Noun.query.filter(Noun.noun_name.like(noun)):
+            reply.append(row.artikel+' '+row.noun_name)
+    else:
+        noun_add(noun)
+        for row in Noun.query.filter(Noun.noun_name.like(noun)):
+            reply.append(row.artikel+' '+row.noun_name)        
+    return(reply)
 
 # print(conj_show('verhuizen', 0))
