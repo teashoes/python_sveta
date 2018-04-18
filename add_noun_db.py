3ea8e68ca27d590e768d5316dbe3552d44be828d
Svetlana <stishkova93@gmail.com>
2018-04-19 00:27:59 +0300

    Update add_noun_db.py

--- add_noun_db.py
+++ add_noun_db.py
@@ -1,8 +1,8 @@
 from database import db_session, Noun
 from noun_artikel import get_art
 # noun='moeder'
-def noun_add(noun_name):   
-    nounadd=Noun(noun=noun_name, artikel=get_art(noun_name))
+def noun_add(noun_n):   
+    nounadd=Noun(noun_name=noun_n, artikel=get_art(noun_n))
     
     db_session.add(nounadd)
     db_session.commit()
