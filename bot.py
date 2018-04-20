5bc318e9bf67513e3280c45269f1827e0b3a10e7
Svetlana <stishkova93@gmail.com>
2018-04-20 21:22:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,31 +5,33 @@ import settings
 def main():
     updater = Updater(settings,TELEGRAM_API_KEY)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("conj", find_verb_form))
-    dp.add_handler(CommandHandler("n", find_noun))
+    dp.add_handler(CommandHandler("noun", find_noun))
     updater.start_polling()
     updater.idle()
 
 def greet_user(bot, update):
-    text = 'Привет! Этот телеграм канал предназначен для людей, изучающих Недерландский язык.\
-     Есть ты хочешь узнать артикль им.существиетльного, введи команду "\noun" и искомое слово.\
+    text = 'Привет! Этот телеграм канал предназначен для людей, изучающих Нидерландский язык.\
+     Есть ты хочешь узнать артикль им.существиетльного, введи команду \ "noun" и искомое слово.\
      Если ты хочешь узнать спряжение глагола, то введи команду и глагол в зависимости от времени спряжения:\
-     "\pr" - настоящее время, "\pa" - прошедшее время, "\pp" - перфект, "\pap" - паст перфект\"'
+     \ "pr" - настоящее время, \ "pa" - прошедшее время, \ "prp" - перфект, \ "pap" - паст перфект, \ "f" - будущее, \
+     \ "cm" - условное наклонение, \ "sm"  - сосглагательное наклонение, \ "cp" - условный перфект.'
     print(text)
     update.message.reply_text(text)
 
 def find_verb_form(bot, update):
     user_text = update.message.text.split(' ')
+    tense_dict={'pr':'0', 'prp':'1', 'pa':'2', 'f':'3', 'cm':'4', 'sm':'5', 'pap':'6', 'cp':'7'}
     if len(user_text)==3:
-        update.message.reply_text("\n".join(conj_show(user_text[1], user_text[2])))
+        update.message.reply_text("\n".join(conj_show(user_text[2], tense_dict.get(user_text[1]))))
     else: 
         update.message.reply_text('?')
 
 def find_noun(bot, update):
     user_text = update.message.text.split(' ')
     if len(user_text) == 2:
-        update.message.reply_text("\n".join(noun_show(user_text[1])))
+        update.message.reply_text(noun_show(user_text[1]))
     else:
         update.message.reply_text('?')
-# main()
+
+main() 
